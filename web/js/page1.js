43a714e8fb6d2fa12aca6f7d70d7a9ff2c668309
Tosiq <pnymeet@gmail.com>
2026-08-11 17:13:24 +0500

    js intro

---
+++ web/js/page1.js
@@ -0,0 +1,25 @@
+// var num = 24
+// console.log(num)
+// var str = "Muhammad Ali"
+// console.log(str);
+// var bool = false;
+// console.log(bool)
+//     keys     key  value  key value
+// var obj = { name: "Ali", age: 24 }
+// document.write(obj.name)
+// indexes     0          1     2   3       4
+// var arr = [  "White",  "Black", 23, 55, { name: "Ali", age: 24 } ]
+// console.log(arr[4])
+// myFunc()
+// function myFunc(){
+//     console.log("Winter is comming...")
+// }
+// var user = null
+// console.log(user)
+
+// var x = 20
+// console.log(x)
+
+// var dt = new Date()
+// console.log(dt)
+
