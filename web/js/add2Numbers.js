ed5362cc090accd337c66325b447b262f7a5834b
Tosiq <pnymeet@gmail.com>
2026-08-18 16:42:32 +0500

    adding 2 numbers

---
+++ web/js/add2Numbers.js
@@ -0,0 +1,9 @@
+// function declaration
+// function calling
+
+function printResult() {
+    var a = Number(document.getElementById("no1").value)
+    var b = Number(document.getElementById("no2").value)
+    var sum = a + b
+    console.log(typeof sum, sum)
+}
