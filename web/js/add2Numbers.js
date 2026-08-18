e7690d7b4a340000b1794a8bd8b26cd6749d987a
Tosiq <pnymeet@gmail.com>
2026-08-18 17:01:54 +0500

    get value by user

--- web/js/add2Numbers.js
+++ web/js/add2Numbers.js
@@ -5,5 +5,7 @@ function printResult() {
     var a = Number(document.getElementById("no1").value)
     var b = Number(document.getElementById("no2").value)
     var sum = a + b
-    console.log(typeof sum, sum)
+
+    var res = document.getElementById("result")
+    res.innerText = sum
 }
