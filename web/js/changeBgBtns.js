3fd1c05c64772659577027d32141f3e88e959bf2
PNY Training <pnymeet@gmail.com>
2026-08-24 05:48:34 -0700

    clr by input / button

---
+++ web/js/changeBgBtns.js
@@ -0,0 +1,8 @@
+function changeBg(clr){
+    document.body.style.backgroundColor = clr
+}
+
+function changeBgByInput() {
+    var myclr = document.getElementById("myclr").value
+    document.body.style.backgroundColor = myclr
+}
