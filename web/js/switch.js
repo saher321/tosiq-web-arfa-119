fb53b1f1bc3c83d5e671c218490df1d633e70e95
PNY Training <pnymeet@gmail.com>
2026-08-24 05:29:30 -0700

    switch case

---
+++ web/js/switch.js
@@ -0,0 +1,18 @@
+function showResult() {
+    var dayNo = 9
+
+    switch(dayNo){
+        case 1:
+            console.log("January")
+            break;
+        case 2:
+            console.log("Feb")
+            break;
+        case 3:
+            console.log("Mar")
+            break;
+        default:
+            console.log("Invalid entry")
+    }
+
+}
