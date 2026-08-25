c00454ea6890e40efb5681f399de6ad9a279ada3
Tosiq <pnymeet@gmail.com>
2026-08-25 03:12:21 -0700

    for loop

---
+++ web/js/forloop.js
@@ -0,0 +1,24 @@
+printResult()
+// declaration variable
+// initialization of variable i=1
+// condition // control flow
+// increament / decreament
+function printResult() {
+    var i
+    // for(i=1; i<=10; i++){
+    //     if(i % 2 == 0){
+    //         console.log(i)
+    //     }
+    // }
+    // for(i=10; i>=1; i--){
+    //     console.log(i)
+    // }
+    for (i=1; i<=10; i++){
+        console.log("2 x " + i + " = " + (2*i) )
+    }
+}
+
+// i=1, 1<=10 :: true   => 1 , i+1
+// i=2, 2<=10 :: true   => 2, i+1
+// i=10, 10<=10 :: true => 10, i+1
+// i=11, 11<=10 :: false => final result
