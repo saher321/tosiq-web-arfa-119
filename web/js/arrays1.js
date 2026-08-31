cc6260f5f3dfbb62b48604d739d5ef0b292d06fc
PNY Training <pnymeet@gmail.com>
2026-08-31 05:17:56 -0700

    arrays

---
+++ web/js/arrays1.js
@@ -0,0 +1,18 @@
+// indexes         0        1        2         3        4
+const colors = ["Black", "White", "Purple", "Orange", "Red"]
+
+// colors.pop()
+colors.push("Brown")
+
+colors.shift()
+colors.unshift("Peach")
+
+console.log(colors)
+
+// console.log("Colors list length", colors.length)
+
+// console.log(colors[3])
+
+for(let i=0; i <= colors.length-1; i++){
+    console.log(colors[i])
+}
