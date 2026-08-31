e35a1335ee3c5c90317af5cde27cc515ae391d63
PNY Training <pnymeet@gmail.com>
2026-08-31 04:34:42 -0700

    let var const

--- web/js/changeBgBtns.js
+++ web/js/changeBgBtns.js
@@ -1,8 +1,16 @@
+applyTheme()
+function applyTheme() {
+    var clr = localStorage.getItem("bg")
+    document.body.style.backgroundColor = clr
+}
+
 function changeBg(clr){
     document.body.style.backgroundColor = clr
+    localStorage.setItem("bg", clr)
 }
 
 function changeBgByInput() {
     var myclr = document.getElementById("myclr").value
     document.body.style.backgroundColor = myclr
+    localStorage.setItem("bg", myclr)
 }
---
+++ web/js/let-var-const.js
@@ -0,0 +1,14 @@
+var a = 12
+var a = "Ahmad"
+// console.log(a)
+// variable redeclared and reinitialized
+
+let b = 12
+b = "Ahmad"
+// console.log(b)
+// reinitialized but not redeclared
+
+const tries = 5
+// tries = 3
+// console.log(tries)
+// cannot reinitialized or redeclared
