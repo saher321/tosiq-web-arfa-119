8ac1594301263dff14cd34420a0f476abff1d4e9
Tosiq <pnymeet@gmail.com>
2026-09-21 17:40:51 +0500

    useEffect

--- web/reactjs/navigations/src/pages/Home.jsx
+++ web/reactjs/navigations/src/pages/Home.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import WebLayout from '../layouts/WebLayout'
 
 const Home = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseData = () => {
+    setCounter(counter + 1)
+  }
+
+  useEffect(() => {
+    if (counter == 5) {
+      console.log("Counter value is set to:", counter)
+    }
+  }, [counter])
+
+  useEffect(() => {
+    console.log("Call only first time")
+  }, [])
+
+  useEffect(() => {
+    console.log("Runs on every click / redender / state change")
+  })
   return (
     <WebLayout>
-      Home page
+      Home page [{counter}]
+      <hr />
+      <div>
+        <button onClick={increaseData}>Click</button>
+      </div>
     </WebLayout>
   )
 }
